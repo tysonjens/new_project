036fa20dd96fe911946ce6a0d0a17dcf96bd0ba7
Tyson Ward <tysonjens@gmail.com>
2020-04-05 15:09:55 -0600

    test_stuff

--- new_project_setup.sh
+++ new_project_setup.sh
@@ -18,8 +18,21 @@ cd code
 git add .
 git commit -m "my message"
 
-## 
+## add new remote repo in github
 
 git remote add origin git@github.com:tysonjens/new_project.git
 git remote -v
 
+## add data to data file
+
+docker-machine create --driver google --google-project projectid --google-zone us-west3-a machinename
+
+docker-machine env machinename
+
+eval $(docker-machine env machinename)
+
+gcloud compute firewall-rules create machine-8000 --action allow --target-tags “docker-machine” --source-ranges “0.0.0.0/0” --rules “tcp[:8000]”
+
+docker run --rm -p 8888:8888 --name new_project_di_1.0 -v "$PWD":/home/jovyan/work jupyter/scipy-notebook:latest
+
+gcloud compute ssh --project $PROJECT_ID --zone $ZONE $INSTANCE_NAME -- -L 8080:localhost:8080
